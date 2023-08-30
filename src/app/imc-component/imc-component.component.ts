046398f633b08571292b492bb372212632e28fb0
leomoraesitu <leomoraesitu@hotmail.com>
2023-08-29 21:44:49 -0300

    antes de instalar o bootstrap

--- src/app/imc-component/imc-component.component.ts
+++ src/app/imc-component/imc-component.component.ts
@@ -6,12 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./imc-component.component.css']
 })
 export class ImcComponentComponent {
-  num1: number=0;
-  num2: number=0;
-  resultado: number=0;
+  peso: number=0;
+  altura: number=0;
+  resultado: number=0;  
+  
 
   calculo(){
-    this.resultado = this.num1 / (this.num2 * this.num2)
+    this.resultado = this.peso / (this.altura * this.altura)  
+    this.imc(); 
+  }
+
+  obesidade: String="";
+
+  imc() {
+    if (this.resultado < 17) {
+        this.obesidade = 'Muito abaixo do peso.';
+    } else if (this.resultado >= 17 && this.resultado < 18.5) {
+      this.obesidade = 'Abaixo do peso.';
+    } else if (this.resultado >= 18.5 && this.resultado < 25) {
+      this.obesidade = 'Peso normal.';
+    } else if (this.resultado >= 25 && this.resultado < 30) {
+      this.obesidade = 'Acima do peso.';
+    } else if (this.resultado >= 30 && this.resultado < 35) {
+      this.obesidade = 'Obesidade I.';
+    } else if (this.resultado >= 35 && this.resultado < 40) {
+      this.obesidade = 'Obesidade II (severa).';
+    } else {
+      this.obesidade = 'Obesidade III (mórbida).';
+    }
   }
 }
 
